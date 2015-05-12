0274e86276f26f1cf8faa42077f8c0b2e84a2d33
ghaoui <ghaoui.hamdi@gmail.com>
2015-05-12 21:40:11 +0200

    modif ch9af

---
+++ supr/head.php
@@ -0,0 +1,69 @@
+<head>
+    
+    <title>CanalBleu admin</title>
+    <meta name="author" content="SuggeElson" />
+    <meta name="description" content="Supr admin template - new premium responsive admin template. This template is designed to help you build the site administration without losing valuable time.Template contains all the important functions which must have one backend system.Build on great twitter boostrap framework" />
+    <meta name="keywords" content="admin, admin template, admin theme, responsive, responsive admin, responsive admin template, responsive theme, themeforest, 960 grid system, grid, grid theme, liquid, masonry, jquery, administration, administration template, administration theme, mobile, touch , responsive layout, boostrap, twitter boostrap" />
+    <meta name="application-name" content="Supr admin template" />
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Le styles -->
+    <!-- Use new way for google web fonts 
+    http://www.smashingmagazine.com/2012/07/11/avoiding-faux-weights-styles-google-web-fonts -->
+    <!-- Headings -->
+    <!-- <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css' />  -->
+    <!-- Text -->
+    <!-- <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css' /> --> 
+    <!--[if lt IE 9]>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Droid+Sans:400" rel="stylesheet" type="text/css" />
+    <link href="http://fonts.googleapis.com/css?family=Droid+Sans:700" rel="stylesheet" type="text/css" />
+    <![endif]-->
+
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/supr-theme/jquery.ui.supr.css" rel="stylesheet" type="text/css" />
+    <link href="css/icons.css" rel="stylesheet" type="text/css" />
+    <!-- Plugin stylesheets -->
+    <link href="plugins/qtip/jquery.qtip.css" rel="stylesheet" type="text/css" />
+    <link href="plugins/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css" />
+    <link href="plugins/jpages/jPages.css" rel="stylesheet" type="text/css" />
+    <link href="plugins/prettify/prettify.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/inputlimiter/jquery.inputlimiter.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/ibutton/jquery.ibutton.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/uniform/uniform.default.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/color-picker/color-picker.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/select/select2.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/validate/validate.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/pnotify/jquery.pnotify.default.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/pretty-photo/prettyPhoto.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/smartWizzard/smart_wizard.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/dataTables/jquery.dataTables.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/elfinder/elfinder.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/plupload/jquery.ui.plupload/css/jquery.ui.plupload.css" type="text/css" rel="stylesheet" />
+    <link href="plugins/search/tipuesearch.css" type="text/css" rel="stylesheet" />
+
+    <!-- Main stylesheets -->
+    <link href="css/main.css" rel="stylesheet" type="text/css" /> 
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/apple-touch-icon-144-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/apple-touch-icon-114-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/apple-touch-icon-72-precomposed.png" />
+    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-57-precomposed.png" />
+    
+    <script type="text/javascript">
+        //adding load class to body and hide page
+        document.documentElement.className += 'loadstate';
+    </script>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
---
+++ supr/script.php
@@ -0,0 +1,58 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap/bootstrap.js"></script>  
+    <script type="text/javascript" src="js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+
+    <!-- Load plugins -->
+    <script type="text/javascript" src="plugins/qtip/jquery.qtip.min.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.grow.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.pie.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.resize.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.tooltip_0.4.4.js"></script>
+    <script type="text/javascript" src="plugins/flot/jquery.flot.orderBars.js"></script>
+
+    <script type="text/javascript" src="plugins/sparkline/jquery.sparkline.min.js"></script>
+    <script type="text/javascript" src="plugins/knob/jquery.knob.js"></script>
+    <script type="text/javascript" src="plugins/fullcalendar/fullcalendar.min.js"></script>
+    <script type="text/javascript" src="plugins/prettify/prettify.js"></script>
+
+    <script type="text/javascript" src="plugins/watermark/jquery.watermark.min.js"></script>
+    <script type="text/javascript" src="plugins/elastic/jquery.elastic.js"></script>
+    <script type="text/javascript" src="plugins/inputlimiter/jquery.inputlimiter.1.3.min.js"></script>
+    <script type="text/javascript" src="plugins/maskedinput/jquery.maskedinput-1.3.min.js"></script>
+    <script type="text/javascript" src="plugins/ibutton/jquery.ibutton.min.js"></script>
+    <script type="text/javascript" src="plugins/uniform/jquery.uniform.min.js"></script>
+    <script type="text/javascript" src="plugins/stepper/ui.stepper.js"></script>
+    <script type="text/javascript" src="plugins/color-picker/colorpicker.js"></script>
+    <script type="text/javascript" src="plugins/timeentry/jquery.timeentry.min.js"></script>
+    <script type="text/javascript" src="plugins/select/select2.min.js"></script>
+    <script type="text/javascript" src="plugins/dualselect/jquery.dualListBox-1.3.min.js"></script>
+    <script type="text/javascript" src="plugins/tiny_mce/jquery.tinymce.js"></script>
+    <script type="text/javascript" src="plugins/validate/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="plugins/search/tipuesearch_set.js"></script>
+    <script type="text/javascript" src="plugins/search/tipuesearch_data.js"></script><!-- JSON for searched results -->
+    <script type="text/javascript" src="plugins/search/tipuesearch.js"></script>
+
+    <script type="text/javascript" src="plugins/animated-progress-bar/jquery.progressbar.js"></script>
+    <script type="text/javascript" src="plugins/pnotify/jquery.pnotify.min.js"></script>
+    <script type="text/javascript" src="plugins/lazy-load/jquery.lazyload.min.js"></script>
+    <script type="text/javascript" src="plugins/jpages/jPages.min.js"></script>
+    <script type="text/javascript" src="plugins/pretty-photo/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="plugins/smartWizzard/jquery.smartWizard-2.0.min.js"></script>
+
+    <script type="text/javascript" src="plugins/ios-fix/ios-orientationchange-fix.js"></script>
+
+    <script type="text/javascript" src="plugins/dataTables/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="plugins/elfinder/elfinder.min.js"></script>
+    <script type="text/javascript" src="plugins/plupload/plupload.js"></script>
+    <script type="text/javascript" src="plugins/plupload/plupload.html4.js"></script>
+    <script type="text/javascript" src="plugins/plupload/jquery.plupload.queue/jquery.plupload.queue.js"></script>
+
+    <!-- Init plugins -->
+    <script type="text/javascript" src="js/statistic.js"></script><!-- Control graphs ( chart, pies and etc) -->
+
+    <!-- Important Place before main.js  -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.21/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="plugins/touch-punch/jquery.ui.touch-punch.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
